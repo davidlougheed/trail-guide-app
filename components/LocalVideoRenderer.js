0ba0be8721f64499873bd27f8a1f5b72171bfa8a
David Lougheed <david.lougheed@gmail.com>
2022-06-02 15:30:00 -0400

    chore: switch to expo-av for rendering video

--- components/LocalVideoRenderer.js
+++ components/LocalVideoRenderer.js
@@ -1,16 +1,18 @@
 // A web interface to manage a trail guide mobile app's content and data.
-// Copyright (C) 2021  David Lougheed
+// Copyright (C) 2021-2022  David Lougheed
 // See NOTICE for more information.
 
-import React from "react";
-import {Platform, View} from "react-native";
-import {WebView} from "react-native-webview";
+import React, {useRef} from "react";
+import {View} from "react-native";
 import {useAssets} from "expo-asset";
+import {Video} from "expo-av";
 
 import assetData from "../data/assets/assets";
-import {getBaseUrlAndFileFromURI, getDataFromAssetURI} from "../utils";
+import {getDataFromAssetURI} from "../utils";
 
 const LocalVideoRenderer = ({style, tnode, ...props}) => {
+    const video = useRef(null);
+
     const {attributes: {width: widthAttr, height: heightAttr}, domNode: {children}} = tnode;
 
     const heightAttrInt = parseInt(heightAttr, 10);
@@ -39,33 +41,11 @@ const LocalVideoRenderer = ({style, tnode, ...props}) => {
     if (error) console.error(error);
     if (!assets) return blankShell;
 
-    const [baseUrl, file] = getBaseUrlAndFileFromURI(assets[0].localUri);
-
-    // expo-av sucks; for now, use a native video tag for web and a WebView for native
-    // TODO: This has since been fixed in expo 44, so we can revert to it
     return <View {...props} style={{height, width}}>
-        {/*<Video source={{uri: assets[0].localUri}}*/}
-        {/*       resizeMode="contain"*/}
-        {/*       useNativeControls={true} />*/}
-        {Platform.OS === "web" ? (
-            <video height={height} width={width} controls={true} preload="auto">
-                <source src={assets[0].localUri} />
-            </video>
-        ) : (
-            <WebView source={{html: `
-                <html lang="en">
-                <head>
-                    <title>Video Element</title>
-                    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0">
-                </head>
-                <body style="margin: 0; padding: 0">
-                <video height="${height}" width="${width}" controls preload="auto">
-                    <source src="${file}" />
-                </video>
-                </body>
-                </html>
-            `, baseUrl: baseUrl}} originWhitelist={["*"]} />
-        )}
+        <Video ref={video}
+               source={assets[0]}
+               resizeMode="contain"
+               useNativeControls={true} />
     </View>;
 };
 
