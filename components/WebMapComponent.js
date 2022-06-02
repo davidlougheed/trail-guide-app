2af48b554f80470ffc5536fc051e7560641dfb5f
David Lougheed <david.lougheed@gmail.com>
2022-06-01 21:07:49 -0400

    chore: fix some issues with web map display; show markers properly

--- components/WebMapComponent.js
+++ components/WebMapComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Dimensions, Text, TouchableOpacity, View} from "react-native";
+import L from "leaflet";
 import {GeoJSON, MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 
 import {transformCoords} from "../gis";
@@ -8,6 +9,22 @@ import layerData from "../data/layers.json";
 import stationData from "../data/stations.json";
 
 import "leaflet/dist/leaflet.css";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconRetina from "leaflet/dist/images/marker-icon-2x.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix issue with marker PNG; see https://stackoverflow.com/questions/49441600/react-leaflet-marker-files-not-found
+// Defaults from https://github.com/Leaflet/Leaflet/blob/main/src/layer/marker/Icon.Default.js
+L.Marker.prototype.options.icon = L.icon({
+    iconUrl: icon,
+    iconRetinaUrl: iconRetina,
+    shadowUrl: iconShadow,
+    iconSize:    [25, 41],
+    iconAnchor:  [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize:  [41, 41],
+});
 
 const WebMapComponent = ({navigation, ...props}) => {
     const height = Dimensions.get("window").height;
@@ -30,9 +47,9 @@ const WebMapComponent = ({navigation, ...props}) => {
             )}
             {stationData.flatMap(t => t.data).filter(station => station.enabled).map(station => {
                 const t = transformCoords(station.coordinates_utm);
-                return <Marker position={[t.latitude, t.longitude]}>
+                return <Marker position={[t.latitude, t.longitude]} key={station.id}>
                     <Popup>
-                        <TouchableOpacity onPress={() => navigation.push(`screen.map.station.${title}`)}>
+                        <TouchableOpacity onPress={() => navigation.push(`screen.map.station.${station.id}`)}>
                             <Text>{station.title}</Text>
                         </TouchableOpacity>
                     </Popup>
