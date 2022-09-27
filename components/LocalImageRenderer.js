f706dde0ce3df7ceb8b859052021f976154235c5
David Lougheed <david.lougheed@gmail.com>
2022-09-26 22:28:33 -0400

    red

--- components/LocalImageRenderer.js
+++ components/LocalImageRenderer.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
     dneStyle: {
         width: 228,
         height: 228,
-        backgroundColor: "#FCFCFC",
+        backgroundColor: "#FCCCCC",
         padding: 8,
     },
     dneTextStyle: {
