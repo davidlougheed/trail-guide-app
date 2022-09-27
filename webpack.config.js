5619a009d9c032babfc0b99d45404d4159ecdace
David Lougheed <david.lougheed@gmail.com>
2022-09-26 22:09:23 -0400

    lint: remove hardcoded webpack production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-  env.mode = "production";
   // Customize the config before returning it.
   // noinspection JSValidateTypes
   const cfg = await createExpoWebpackConfigAsync({
