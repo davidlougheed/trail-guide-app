7ec9f04cedc7acce8e7693aeded4395b260b8457
David Lougheed <david.lougheed@gmail.com>
2024-02-29 19:47:10 -0500

    fix: add expo-crypto to fix build issues

--- webpack.config.js
+++ webpack.config.js
@@ -15,5 +15,9 @@ module.exports = async function (env, argv) {
     ...cfg.optimization,
     usedExports: true,  // TODO: this is false normally, but seems to cause a crash with react-native-render-html
   };
+  cfg.resolve.fallback = {
+    ...cfg.resolve.fallback,
+    crypto: require.resolve("expo-crypto"),
+  };
   return cfg;
 };
